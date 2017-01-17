d43fb7f9616f0269f2be970d7ab13925b2298b54
ming820214 <story3@163.com>
2017-01-17 16:37:09 +0800

    CHP积分管理模块上线。

---
+++ Application/Home/Controller/ChpController.class.php
@@ -0,0 +1,323 @@
+<?php
+namespace Home\Controller;
+
+class ChpController extends HomeController {
+
+	private $pageNumber=0;
+	private $pageCount=10;
+	
+	public function index(){
+		
+		$mod = M('chpInfo');
+		$condition['is_del'] = 1; //正常记录
+		
+        $list=$mod->where($condition)->limit($pageNumber,$pageCount)->select();
+		
+       
+        foreach ($list as &$v) {//跟踪人
+        	if($v['record_type'] == 1){
+        		$v['record_type'] = "积分";
+        	}else if($v['record_type'] == 2){
+        		$v['record_type'] = "兑换";
+        	}
+        	 
+        	if($v['flag'] == 1){
+        		$v['flag'] = "申请积分兑换";
+        	}else if($v['flag'] == 2){
+        		$v['flag'] = "兑换完成";
+        	}else{
+        		$v['flag'] = null;
+        	}
+        	 
+        	$v['scheme'] = M('chpDictionary')->where(['id'=>$v['scheme']])->getField('name');
+        	$v['item1'] = M('chpDictionary')->where(['id'=>$v['item1']])->getField('name');
+        	if($v['item2']){
+        		$v['item2'] = M('chpDictionary')->where(['id'=>$v['item2']])->getField('name');
+        	}else{
+        		$v['item2'] = null;
+        	}
+        	 
+        	$v['user_id'] = M('hw003.person_all',null)->where(['id'=>$v['user_id']])->getField('name');
+        	 
+        	$v['creator']=M('user')->where(['id'=>$v['creator']])->getField('name');
+        
+        	$v['updator'] = M('user')->where(['id'=>$v['updator']])->getField('name');
+        	 
+        }
+        
+        $this->list=$list;
+        $this->maxCount=$list?count($list):1;//显示全部数据
+		
+        //集团所有员工信息列表
+        $user_list = M('hw003.person_all',null)->where("state=1")->group("school,position,name")->getField('id,school,position,name');
+        
+        $this->user_list = $user_list;
+        
+        //CHP积分方案列表
+        $scheme_list = M('chpDictionary')->where("pid=0 and is_del=1 and dept_id = " . session('dept_id'))->order('sort')->select();
+		
+        $this->scheme_list = $scheme_list;
+        
+		$this->display();
+	}
+	
+	//获取相关方案下所属积分项
+	public  function getItems(){
+		$pid = isset($_POST['pid'])?$_POST['pid']:null;
+		$item_list = M('chpDictionary')->where(['pid'=>$pid,'is_del'=>1])->order('sort')->select();
+		
+		if(IS_AJAX && $item_list){
+			// 发送给页面的数据
+			$this->ajaxReturn([
+			
+					'state'=>'ok',//查询结果
+					'data'=>$item_list
+			
+			]);
+		}else{
+			return null;
+		}
+		
+	}
+	
+	
+	//获取单条或多条信息
+    public function pager($id=0){
+            
+    	$mod=M('chpInfo');
+        
+    
+        if(IS_AJAX && I('get.pageCount')){
+            $page=I('get.pageNumber');//请求第几条开始
+            $page_count=I('get.pageCount');//一页多少条记录
+
+            $condition=I('post.');//获取json查询条件转换成php数组
+            
+            $condition['is_del'] = 1; //有效数据; 
+            
+            if($condition['keyword']){
+            	$condition['descp'] = array('like', "%" . $condition['keyword'] . "%");
+            }
+            	
+			
+			if($condition['begin'] && $condition['end']){
+				$condition['worth'] = array('between',array($condition['begin'],$condition['end']));
+			}else if($condition['begin']){
+				$condition['worth'] = array('egt',$condition['begin']);
+			}else if($condition['end']){
+				$condition['worth'] = array('elt',$condition['end']);
+			}
+			
+			
+            array_empty_delt($condition);
+    		$count=$mod->where($condition)->count();//满足条件的记录总数
+    		$data=$mod->where($condition)->limit($page,$page_count)->order('create_time desc,update_time desc')->select();//获取到数据
+        }
+
+        foreach ($data as &$v) {//跟踪人
+        	if($v['record_type'] == 1){
+        		$v['record_type'] = "积分";
+        	}else if($v['record_type'] == 2){
+        		$v['record_type'] = "兑换";
+        	}
+        	
+        	if($v['flag'] == 1){
+        		$v['flag'] = "申请积分兑换";
+        	}else if($v['flag'] == 2){
+        		$v['flag'] = "兑换完成";
+        	}else{
+        		$v['flag'] = null;
+        	}
+        	
+        	$v['scheme'] = M('chpDictionary')->where(['id'=>$v['scheme']])->getField('name');
+        	$v['item1'] = M('chpDictionary')->where(['id'=>$v['item1']])->getField('name');
+        	if($v['item2']){
+        		$v['item2'] = M('chpDictionary')->where(['id'=>$v['item2']])->getField('name');
+        	}else{
+        		$v['item2'] = null;
+        	}
+        	
+        	$v['user_id'] = M('hw003.person_all',null)->where(['id'=>$v['user_id']])->getField('name');
+        	
+            $v['creator']=M('user')->where(['id'=>$v['creator']])->getField('name');
+            
+			$v['updator'] = M('user')->where(['id'=>$v['updator']])->getField('name');
+           
+        }
+        
+        
+        if($id){
+        	$data[0]=$mod->find($id);//获取到数据
+        	$count=1;
+        }
+
+		// 发送给页面的数据
+		$this->ajaxReturn([
+
+			'state'=>'ok',//查询结果
+			'maxCount'=>$count,//查询到数据库有多少条满足条件记录
+			'data'=>$data
+
+		  ]);
+    
+    }
+
+	public function addChpInfo(){
+		$mod=M('chpInfo');
+        $mod->create();
+        if(I('post.id')){
+        	$mod->updator=session('auth_id');
+        	$mod->update_time = date('Y-m-d H:i:s');
+            if($mod->save()){
+            	$this->ajaxReturn('ok');
+			}
+        }else{
+            $mod->record_type=1;
+            $mod->creator=session('auth_id');
+            $mod->create_time = date('Y-m-d H:i:s');
+            $mod->is_del = 1;
+            $condition['descp']=I('post.descp');
+            $repeat=$mod->where($condition)->find();
+            if($repeat){
+            	
+            	$this->ajaxReturn('录入有重复……   ：    积分描述： ' . $repeat['descp']);
+			}
+	    	if($mod->add()) {
+	    		$this->ajaxReturn('ok');
+			}
+        }
+	}
+	
+	
+	public function delChpInfo(){
+		$mod=M('chpInfo');
+		$mod->create();
+		if(I('post.id')){
+			
+			$condition['id']=I('post.id');
+			$repeat=$mod->where($condition)->find();
+			if($repeat){
+				$mod->updator=session('auth_id');
+				$mod->update_time = date('Y-m-d H:i:s');
+				$mod->is_del = 2;
+				if($mod->save()){
+					$this->ajaxReturn('ok');
+				}
+			}
+			
+		}else{
+			$this->ajaxReturn('该积分记录不存在，请于系统管理员联系！');
+		}
+	}
+	
+	
+	public function doReceiveInfo(){
+		$mod=M('chpInfo');
+		$mod->create();
+		if(I('post.id')){
+				
+			$condition['id']=I('post.id');
+			$repeat=$mod->where($condition)->find();
+			if($repeat && $repeat['flag'] == 1){
+				$mod->updator=session('auth_id');
+				$mod->update_time = date('Y-m-d H:i:s');
+				$mod->flag = 2;
+				if($mod->save()){
+					$this->ajaxReturn('ok');
+				}
+			}else{
+				$this->ajaxReturn('非积分兑换申请记录，请勿进行积分兑换确认操作！');
+			}
+				
+		}else{
+			$this->ajaxReturn('该积分记录不存在，请于系统管理员联系！');
+		}
+	}
+	
+/**
+数据导入
+*/
+    public function import(){
+        if(IS_POST){
+            if($_FILES['file']['type']!='application/vnd.ms-excel'||!strstr($_FILES['file']['name'],'.csv')){
+                $this->success('文件格式不正确，<br/>请使用提供的模版导入！'); die;
+            }
+            // var_dump($_FILES['file']);die;
+            $file = fopen($_FILES['file']['tmp_name'], "r");
+            while ($data = fgetcsv($file)) {
+                $list[] = $data;
+             }
+            if($list[0][0]!='卡片编号')$zz=true;
+            if(count($list[0])!=5)die('数据格式出错');
+            fclose($file);
+             foreach ($list as $k => $v) {
+                if($k<1)continue;
+                if($zz)
+                foreach ($v as &$val) {
+                    $val=$this->characett($val);
+                }
+
+				switch(trim($v[1])){
+					case "黑卡": $v[1] = '01';break;
+					case "金卡": $v[1] = '02';break;
+					case "银卡": $v[1] = '03';break;
+				}
+				
+                $dat[]=[
+                    'card_no'=>$v[0],
+                    'card_state'=>'02',
+                    'card_type'=> $v[1],
+                    'card_value'=> $v[2],
+                    'card_owner'=> $v[3],
+                    'owner_tel'=>$v[4],
+                    'card_school' =>session('school_id'),
+                    'del'=>0,
+                    'creator'=>session('auth_id')
+                   ];
+             }
+             // 数据去内部重复
+             foreach ($dat as $k=>$v) {
+                if($v['card_no'])
+                if(in_array($v['card_no'],$tmp)){
+                    unset($dat[$k]);
+                }else{
+                    $tmp[]=$v['card_no'];
+                }
+             }
+            // 数据库里查找是否有重复
+            $m=M('vipcard')->field('card_no')->distinct(true)->select();
+            $m=array_column($m,'card_no');
+           
+            foreach ($dat as $k => $v) {
+                if(in_array($v['card_no'],$m))unset($dat[$k]);
+            }
+            // var_dump($dat);die;
+            M('vipcard')->addAll($dat);
+            $this->success('导入成功了'.count($dat).'条记录');
+        }
+    }
+
+    //输出导入使用的模版文件
+    public function import_template(){
+
+        $output='卡片编号,卡片类型,卡片价值,持卡人,持卡人电话';
+        header("Content-type:application/vnd.ms-excel");
+        header("Content-disposition: attachment;filename=VIP卡数据导入模版文件.csv");
+        header("Cache-control: private");
+        header("Pragma: private");
+        print($output);
+
+    }
+
+    //自动识别文件编码并转换为utf-8,（有问题判断）
+    function characett($data){
+      if( !empty($data) ){    
+        $fileType = mb_detect_encoding($data , ['GBK','UTF-8','ISO-8859-1','UTF-32']) ;   
+        if( $fileType != 'UTF-8'){   
+          $data = mb_convert_encoding($data ,'utf-8' , $fileType);   
+        }
+      } 
+      return $data;    
+    }
+	
+}
---
+++ Application/Home/Controller/ChpDictionaryController.class.php
@@ -0,0 +1,211 @@
+<?php
+namespace Home\Controller;
+
+class ChpDictionaryController extends HomeController {
+    protected $config = array('app_type' => 'master','admin'=>'FinanceType');
+
+    public function index() {
+        $node = M("ChpDictionary");
+        if (!empty($_POST['eq_pid'])) {
+            $eq_pid = $_POST['eq_pid'];
+        } elseif (!empty($_GET['eq_pid'])) {
+            $eq_pid = $_GET['eq_pid'];
+        } else {
+            $eq_pid = $node -> where('pid=0 and is_del=1') -> order('sort asc') -> getField('id');
+        }
+
+        $this -> assign('eq_pid', $eq_pid);
+
+        $list = $node -> where('pid=0 and is_del=1') -> order('sort asc') -> getField('id,name');
+        $this -> assign('groupList', $list);
+
+        $menu = array();
+        $menu = $node -> where('is_del=1')  -> field('id,pid,name') -> order('sort asc') -> select();
+        $tree = $this->list_to_tree($menu, $eq_pid);
+
+        $model = M("ChpDictionary");
+        $list = $model -> where('is_del=1')  -> order('sort asc') -> getField('id,name');
+        $this -> assign('node_list', $list);
+        $this -> assign('menu', $this->popup_tree_menu($tree));
+        $this -> display("index2");
+    }
+
+    public function read($id){
+        $model = M('ChpDictionary');
+        $vo = $model  -> where('is_del=1') -> find($id);
+        /* if($vo['dept_id']){
+            $vo['dept_name'] = get_dept_name($vo['dept_id']);
+        }
+
+        if($vo['dept_id2']){
+            $vo['dept_name2'] = get_dept_name($vo['dept_id2']);
+        } */
+
+        if (IS_AJAX) {
+            if ($vo !== false) {// 读取成功
+                $return['data'] = $vo;
+                $return['status'] = 1;
+                $return['info'] = "读取成功";
+                $this -> ajaxReturn($return);
+            } else {
+                $return['status'] = 0;
+                $return['info'] = "读取错误";
+                $this -> ajaxReturn($return);
+            }
+        }
+        $this -> assign('vo', $vo);
+        $this -> display();
+        return $vo;
+    }
+	
+    //winpop显示，防止出现错误提示；
+    public function add(){
+    	$this->display();
+    }
+    
+    
+    protected function _insert($name='ChpDictionary') {
+        $model = D('ChpDictionary');
+        if (false === $model -> create()) {
+            $this -> error($model -> getError());
+        }
+        if (strpos($model -> url, '##') !== false) {
+            $model -> sub_folder = ucfirst(get_controller(str_replace("##", "", $model -> url))) . "Folder";
+        } else {
+            $model -> sub_folder = '';
+        }
+        //保存当前数据对象
+        $list = $model -> add();
+        if ($list !== false) {//保存成功
+            $this -> assign('jumpUrl', get_return_url());
+            $this -> success('新增成功!');
+        } else {
+            //失败提示
+            $this -> error('新增失败!');
+        }
+    }
+
+    protected function _update($name='ChpDictionary'){
+        $id = $_POST['id'];
+        $model = D("ChpDictionary");
+        if (false === $model -> create()) {
+            $this -> error($model -> getError());
+        }
+        if (strpos($model -> url, '##') !== false) {
+            $model -> sub_folder = ucfirst(get_controller(str_replace("##", "", $model -> url))) . "Folder";
+        } else {
+            $model -> sub_folder = '';
+        }
+        // 更新数据
+        $list = $model -> save();
+        if (false !== $list) {
+            //成功提示
+            $this -> assign('jumpUrl', get_return_url());
+            $this -> success('编辑成功!');
+        } else {
+            //错误提示
+            $this -> error('编辑失败!');
+        }
+    }
+
+    function winpop() {
+        $menu = D("ChpDictionary") -> order('sort asc') -> select();
+        $tree = $this->list_to_tree($menu);
+        $this -> assign('menu', $this->popup_tree_menu($tree));
+        $this -> display();
+    }
+
+    function del($id){
+        $where['pid']=array('eq',$id);
+        $list=M("ChpDictionary")->where($where)->select();
+
+        if($list){
+            $this->error('有子节点不能删除');
+        }else{
+        	$mo = M("ChpDictionary")->where("id=" . $id)->find();
+        	if($mo){
+        		$mo['is_del'] = 2;
+        		if(M("ChpDictionary")->save($mo)){
+        			$this->success('删除成功', 'ChpDictionary/index');
+        		}
+        	}
+        }
+       /*  $model = M("RoleNode");
+        $where['node_id'] = $id;
+        $model -> where($where) -> delete();
+        $this -> _destory($id); */
+    }
+
+    function list_to_tree($list, $root = 0, $pk = 'id', $pid = 'pid', $child = '_child') {
+        // 创建Tree
+        $tree = array();
+        if (is_array($list)) {
+            // 创建基于主键的数组引用
+            $refer = array();
+            foreach ($list as $key => $data) {
+                $refer[$data[$pk]] = &$list[$key];
+            }
+            foreach ($list as $key => $data) {
+                // 判断是否存在parent
+                $parentId = 0;
+                if (isset($data[$pid])) {
+                    $parentId = $data[$pid];
+                }
+                if ((string)$root == $parentId) {
+                    $tree[] = &$list[$key];
+                } else {
+                    if (isset($refer[$parentId])) {
+                        $parent = &$refer[$parentId];
+                        $parent[$child][] = &$list[$key];
+                    }
+                }
+            }
+        }
+        return $tree;
+    }
+
+    public function popup_tree_menu($tree, $level = 0) {
+        $level++;
+        $html = "";
+        if (is_array($tree)) {
+            $html = "<ul class=\"tree_menu\">\r\n";
+            foreach ($tree as $val) {
+                if (isset($val["name"])) {
+                    $title = $val["name"];
+                    $id = $val["id"];
+                    if (empty($val["id"])) {
+                        $id = $val["name"];
+                    }
+                    if (!empty($val["is_del"])) {
+                        $del_class = "is_del";
+                    } else {
+                        $del_class = "";
+                    }
+                    if (isset($val['_child'])) {
+                        $html = $html . "<li>\r\n<a data-level=\"{$level}\" class=\"$del_class\" node=\"$id\" ><i class=\"fa fa-angle-right level$level\"></i><span>$title</span></a>\r\n";
+                        $html = $html . $this->popup_tree_menu($val['_child'], $level);
+                        $html = $html . "</li>\r\n";
+                    } else {
+                        $html = $html . "<li>\r\n<a data-level=\"{$level}\" class=\"$del_class\" node=\"$id\" ><i class=\"fa fa-angle-right level$level\"></i><span>$title</span></a>\r\n</li>\r\n";
+                    }
+                }
+            }
+            $html = $html . "</ul>\r\n";
+        }
+        return $html;
+    }
+
+    function depts() {
+        $plugin['jquery-ui'] = true;
+        $this -> assign("plugin", $plugin);
+        $model = M("Dept");
+        $list = array();
+        $list = $model -> where('is_del=0') -> field('id,pid,name') -> order('sort asc') -> select();
+        $list = list_to_tree($list);
+        $this -> assign('list_dept', popup_tree_menu($list));
+        $this -> assign('xid', $_GET['xid']);
+        $this -> assign('xname', $_GET['xname']);
+        $this -> display();
+        return;
+    }
+}
