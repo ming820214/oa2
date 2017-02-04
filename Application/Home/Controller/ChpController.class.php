55c3c8e25f9f2b8b693f3fa7014dd5368edef268
ming820214 <story3@163.com>
2017-02-04 14:33:44 +0800

    增加兑换退回功能及积分审核历史查询功能。

--- Application/Home/Controller/ChpController.class.php
+++ Application/Home/Controller/ChpController.class.php
@@ -30,9 +30,11 @@ class ChpController extends HomeController {
         	}else if($v['flag'] == 2){
         		$v['flag'] = "兑换完成";
         	}else{
-        		$v['flag'] = null;
+        		$v['flag'] = '';
         	}
-        	 
+        	if($v['worth']<0){
+        		$v['worth'] = -$v['worth']; 
+        	}
         	$v['scheme'] = M('chpDictionary')->where(['id'=>$v['scheme']])->getField('name');
         	$v['item1'] = M('chpDictionary')->where(['id'=>$v['item1']])->getField('name');
         	if($v['item2']){
@@ -144,7 +146,7 @@ class ChpController extends HomeController {
         	}else if($v['flag'] == 2){
         		$v['flag'] = "兑换完成";
         	}else{
-        		$v['flag'] = null;
+        		$v['flag'] = '';
         	}
         	
         	$v['scheme'] = M('chpDictionary')->where(['id'=>$v['scheme']])->getField('name');
@@ -203,7 +205,7 @@ class ChpController extends HomeController {
             			
             		$wx= getWechatObj();
             		$wx->sendNewsMsg(
-            				[$wx->buildNewsItem("请注意：您有一项积分活动被更新！",$info,wx_oauth(C('WWW').'/oa_old/weixin.php/chp/chp'),'')],
+            				[$wx->buildNewsItem("请注意：您有一项CHP积分活动被更新！",$info,wx_oauth(C('WWW').'/oa_old/weixin.php/chp/chp'),'')],
             				['touser'=>$user],
             				C('WECHAT_APP')['TZTX']
             				);
@@ -240,7 +242,7 @@ class ChpController extends HomeController {
 	    				
 	    			$wx= getWechatObj();
 	    			$wx->sendNewsMsg(
-	    					[$wx->buildNewsItem("恭喜您，您又获得了积分",$info,wx_oauth(C('WWW').'/oa_old/weixin.php/chp/chp'),'')],
+	    					[$wx->buildNewsItem("恭喜您，您又获得了CHP积分",$info,wx_oauth(C('WWW').'/oa_old/weixin.php/chp/chp'),'')],
 	    					['touser'=>$user],
 	    					C('WECHAT_APP')['TZTX']
 	    					);
@@ -265,7 +267,11 @@ class ChpController extends HomeController {
 	public function delChpInfo(){
 		$mod=M('chpInfo');
 		$mod->create();
-		//$web_chat = $mod->user_id;
+		
+		if($mod->reason){
+			$reason = $mod->reason;
+		}
+		
 		if(I('post.id')){
 			
 			$condition['id']=I('post.id');
@@ -273,7 +279,14 @@ class ChpController extends HomeController {
 			if($repeat){
 				$mod->updator=session('auth_id');
 				$mod->update_time = date('Y-m-d H:i:s');
-				$mod->is_del = 2;
+				
+				if($repeat['flag']){
+					$mod->reason = $reason;
+					$mod->is_del = 3;
+				}else{
+					$mod->is_del = 2;
+				}
+				
 				if($mod->save()){
 					
 					{
@@ -289,12 +302,21 @@ class ChpController extends HomeController {
 						$info='点击可直接进入查看……';
 						 
 						$wx= getWechatObj();
-						$wx->sendNewsMsg(
-								[$wx->buildNewsItem("请注意：您有一项积分被删除！",$info,wx_oauth(C('WWW').'/oa_old/weixin.php/chp/chp'),'')],
-								['touser'=>$user],
-								C('WECHAT_APP')['TZTX']
-								);
-					
+						
+						if($repeat['flag']){
+							$wx->sendNewsMsg(
+									[$wx->buildNewsItem("请注意：您有一项CHP积分兑换被退回！",$info,wx_oauth(C('WWW').'/oa_old/weixin.php/chp/chp'),'')],
+									['touser'=>$user],
+									C('WECHAT_APP')['TZTX']
+									);
+						}else{
+							$wx->sendNewsMsg(
+									[$wx->buildNewsItem("请注意：您有一项CHP积分被删除！",$info,wx_oauth(C('WWW').'/oa_old/weixin.php/chp/chp'),'')],
+									['touser'=>$user],
+									C('WECHAT_APP')['TZTX']
+									);
+						}
+						
 					}
 					
 					$this->ajaxReturn('ok');
@@ -335,7 +357,7 @@ class ChpController extends HomeController {
 							
 						$wx= getWechatObj();
 						$wx->sendNewsMsg(
-								[$wx->buildNewsItem("恭喜您：您有一份积分兑换已通过！",$info,wx_oauth(C('WWW').'/oa_old/weixin.php/chp/chp'),'')],
+								[$wx->buildNewsItem("恭喜您：您有一份CHP积分兑换已通过！",$info,wx_oauth(C('WWW').'/oa_old/weixin.php/chp/chp'),'')],
 								['touser'=>$user],
 								C('WECHAT_APP')['TZTX']
 								);
@@ -352,6 +374,169 @@ class ChpController extends HomeController {
 		}
 	}
 	
+	
+	public function distory(){
+		$mod = M('chpInfo');
+		$condition['is_del'] = array('in','2,3'); //正常记录
+		
+		$dic = M("chpDictionary")->where(['dept_id'=>session('dept_id'),'leaf'=>1])->getField('id',true);
+		
+		$condition['item1|item2'] = array('in',$dic);
+		
+		$list=$mod->where($condition)->limit($pageNumber,$pageCount)->select();
+		
+		 
+		foreach ($list as &$v) {//跟踪人
+			if($v['record_type'] == 1){
+				$v['record_type'] = "积分";
+			}else if($v['record_type'] == 2){
+				$v['record_type'] = "兑换";
+			}
+			
+			if($v['is_del'] != 3){
+				if($v['flag'] == 1){
+					$v['flag'] = "申请积分兑换";
+				}else if($v['flag'] == 2){
+					$v['flag'] = "兑换完成";
+				}else{
+					$v['flag'] = '';
+				}	
+			}else{
+				$v['flag'] = '积分兑换退回';
+			}
+			
+			if($v['worth']<0){
+				$v['worth'] = -$v['worth'];
+			}
+			$v['scheme'] = M('chpDictionary')->where(['id'=>$v['scheme']])->getField('name');
+			$v['item1'] = M('chpDictionary')->where(['id'=>$v['item1']])->getField('name');
+			if($v['item2']){
+				$v['item2'] = M('chpDictionary')->where(['id'=>$v['item2']])->getField('name');
+			}else{
+				$v['item2'] = null;
+			}
+		
+			$v['user_id'] = M('hw003.person_all',null)->where(['id'=>$v['user_id']])->getField('name');
+		
+			$v['creator']=M('user')->where(['id'=>$v['creator']])->getField('name');
+		
+			$v['updator'] = M('user')->where(['id'=>$v['updator']])->getField('name');
+		
+		}
+		
+		$this->list=$list;
+		$this->maxCount=$list?count($list):1;//显示全部数据
+		
+		//集团所有员工信息列表
+		$user_list = M('hw003.person_all',null)->where("state=1")->group("school,position,name")->getField('id,school,position,name');
+		
+		$this->user_list = $user_list;
+		
+		//CHP积分方案列表
+		$scheme_list = M('chpDictionary')->where("pid=0 and is_del=1 and `group` = 1 and dept_id = " . session('dept_id'))->order('sort')->select();
+		
+		$this->scheme_list = $scheme_list;
+		
+		$this->display();
+	}
+	
+	
+	public function distory_pager($id=0){
+		$mod=M('chpInfo');
+		
+		
+		if(IS_AJAX && I('get.pageCount')){
+			$page=I('get.pageNumber');//请求第几条开始
+			$page_count=I('get.pageCount');//一页多少条记录
+		
+			$condition=I('post.');//获取json查询条件转换成php数组
+			if(!$condition['is_del']){
+				$condition['is_del'] = array('in','2,3'); //有效数据;
+			}
+		
+			if($condition['keyword']){
+				$condition['descp'] = array('like', "%" . $condition['keyword'] . "%");
+			}
+			 
+				
+			if($condition['begin'] && $condition['end']){
+				$condition['worth'] = array('between',array($condition['begin'],$condition['end']));
+			}else if($condition['begin']){
+				$condition['worth'] = array('egt',$condition['begin']);
+			}else if($condition['end']){
+				$condition['worth'] = array('elt',$condition['end']);
+			}
+				
+				
+			if($condition['beginDate'] && $condition['endDate']){
+				$condition['exchange_time'] = array('between',array($condition['beginDate'],$condition['endDate']));
+			}else if($condition['beginDate']){
+				$condition['exchange_time'] = array('egt',$condition['beginDate']);
+			}else if($condition['endDate']){
+				$condition['exchange_time'] = array('elt',$condition['endDate']);
+			}
+				
+				
+			$dic = M("chpDictionary")->where(['dept_id'=>session('dept_id'),'leaf'=>1])->getField('id',true);
+				
+			$condition['item1|item2'] = array('in',$dic);
+				
+			array_empty_delt($condition);
+			$count=$mod->where($condition)->count();//满足条件的记录总数
+			$data=$mod->where($condition)->limit($page,$page_count)->order('create_time desc,update_time desc')->select();//获取到数据
+		}
+		
+		foreach ($data as &$v) {//跟踪人
+			if($v['record_type'] == 1){
+				$v['record_type'] = "积分";
+			}else if($v['record_type'] == 2){
+				$v['record_type'] = "兑换";
+				$v['worth'] = -$v['worth'];
+			}
+			 
+			if($v['is_del'] != 3){
+				if($v['flag'] == 1){
+					$v['flag'] = "申请积分兑换";
+				}else if($v['flag'] == 2){
+					$v['flag'] = "兑换完成";
+				}else{
+					$v['flag'] = '';
+				}
+			}else{
+				$v['flag'] = '积分兑换退回';
+			}
+			 
+			$v['scheme'] = M('chpDictionary')->where(['id'=>$v['scheme']])->getField('name');
+			$v['item1'] = M('chpDictionary')->where(['id'=>$v['item1']])->getField('name');
+			if($v['item2']){
+				$v['item2'] = M('chpDictionary')->where(['id'=>$v['item2']])->getField('name');
+			}else{
+				$v['item2'] = null;
+			}
+			 
+			$v['user_id'] = M('hw003.person_all',null)->where(['id'=>$v['user_id']])->getField('name');
+			 
+			$v['creator']=M('user')->where(['id'=>$v['creator']])->getField('name');
+		
+			$v['updator'] = M('user')->where(['id'=>$v['updator']])->getField('name');
+			 
+		}
+		
+		
+		if($id){
+			$data[0]=$mod->find($id);//获取到数据
+			$count=1;
+		}
+		
+		// 发送给页面的数据
+		$this->ajaxReturn([
+		
+				'state'=>'ok',//查询结果
+				'maxCount'=>$count,//查询到数据库有多少条满足条件记录
+				'data'=>$data
+		
+		]);
+	}
 /**
 数据导入
 */
