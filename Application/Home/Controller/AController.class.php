77b663b4c2460b6f4398701a91c77693597a69b8
ming820214 <story3@163.com>
2017-04-15 15:41:03 +0800

    no message

--- Application/Home/Controller/AController.class.php
+++ Application/Home/Controller/AController.class.php
@@ -29,6 +29,25 @@ class AController extends HomeController {
         $this->display(); // 输出模板
     }
 
+    public function search(){
+    	$w = $_POST;
+    	if (!empty($_POST['keyword'])) {
+    		$w['school|user|position'] = array('like', "%" . $_POST['keyword'] . "%");
+    	}
+    	$w = array_filter($w);
+    	$s=M('hw001.school',null)->select();
+    	$this->school=$s;
+    	$User =M('hw001.user',null); // 实例化User对象
+    	$count      = $User->where($w)->count();// 查询满足要求的总记录数
+    	$Page       = new \Home\Hongwen\Page($count,25);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+    	$show       = $Page->show();// 分页显示输出
+    	// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+    	$list = $User->where($w)->order('school asc,position desc,user asc')->limit($Page->firstRow.','.$Page->listRows)->select();
+    	$this->assign('item',$list);// 赋值数据集
+    	$this->assign('page',$show);// 赋值分页输出
+    	$this->display("p"); // 输出模板
+    }
+    
     public function delt(){
         if($_GET['id'])
             $w['id']=$_GET['id'];
