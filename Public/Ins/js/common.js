0b53813104792b2aeab60ef9a3d81a82cb0875d4
ming <ming820214@gmail.com>
2017-08-15 14:24:50 +0800

    设计需求流程设计

--- Public/Ins/js/common.js
+++ Public/Ins/js/common.js
@@ -848,6 +848,7 @@ Ycom = {
                           input[type=text][name='+x+'],\
                           input[type=date][name='+x+'],\
                           input[type=number][name='+x+'],\
+                          input[type=checkbox][name="'+x+'[]"],\
                           textarea[name='+x+'],\
                           select[name="'+x+'[]"],\
                           select[name='+x+']').not(ignore).val(data[x]);
@@ -863,6 +864,7 @@ Ycom = {
         $(form).find('input[type=hidden],\
                       input[type=text],\
                       input[type=number],\
+        			  input[type=file],\
                       textarea').not(ignore).val('');
         $(form).find('select').find('option:first').not(ignore).prop('selected','selected');
             // TODO radio checked。。。
